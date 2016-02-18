8ecf3fd928cfff6c0684f0fa2580431f2ffa29e9
nmbryant <nmbryant@wpi.edu>
2016-02-18 14:18:53 -0500

    Clicking in the field now moves the soccer ball to that position

--- MiddlewareProject/Assets/Goalie/SoccerBall.cs
+++ MiddlewareProject/Assets/Goalie/SoccerBall.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using RAIN.Core;
+using RAIN.Serialization;
 
 public class SoccerBall : MonoBehaviour {
 
-    // Use this for initialization
-    void Start () {
-	    
-	}
-	
+    private AIRig m_soccerBallRig;
+
+    private Vector3 m_targetPosition;
+
+    void Start()
+    {
+        m_soccerBallRig = GetComponent<AIRig>();
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            _GetMovementLoc();
+        }
+    }
+
+    private void _GetMovementLoc()
+    {
+        // Get the position of the click in the game world
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction, Color.black, 10000);
+        Plane xy = new Plane(Vector3.up, new Vector3(0, 0, 0));
+        float point;
+        xy.Raycast(ray, out point);
+        m_targetPosition = ray.GetPoint(point);
+        m_targetPosition.y = transform.position.y;
+        m_soccerBallRig.AI.WorkingMemory.SetItem<Vector3>("MovePos", m_targetPosition);
+    }
 }
